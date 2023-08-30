a3d725fbd1cb6b66677295ee582afb3cd496569b
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-31 00:56:13 +0300

    Wrapped components with redux provider

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
+import { Provider } from 'react-redux';
+import Store from './store/store';
+import Header from './components/header';
+import Footer from './components/footer';
+import List from './components/list';
+
 function App() {
   return (
-    <div>
-      Redux Thunk
-    </div>
+    <Provider store={Store}>
+      <Header />
+      <main>
+        <List />
+      </main>
+      <Footer />
+    </Provider>
   );
 }
 
